f28bac16ed131a547e37553c329d77bbebf1ff0b
McGuigan, Ruairi (ISBANUK) <ruairi.mcguigan@gmail.com>
2017-09-13 23:02:29 +0100

    list set up with mock data

--- app/src/main/java/market/stock/demo/rx/rxstockmarket/MainActivity.java
+++ app/src/main/java/market/stock/demo/rx/rxstockmarket/MainActivity.java
@@ -1,13 +1,44 @@
 package market.stock.demo.rx.rxstockmarket;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello_world_salute)
+    TextView helloText;
+
+    @BindView(R.id.stock_updates_recycler_view)
+    RecyclerView recyclerView;
+
+    private LinearLayoutManager layoutManager;
+    private StockDataAdapter stockDataAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initList();
+
+        Observable.just("google","apple","android", "audi").subscribe(s -> stockDataAdapter.add(s));
+    }
+
+    private void initList() {
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        stockDataAdapter = new StockDataAdapter();
+        recyclerView.setAdapter(stockDataAdapter);
     }
 }
